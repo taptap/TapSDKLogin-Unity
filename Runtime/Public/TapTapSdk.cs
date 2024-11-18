5ab62de515caf44b791a75fc6c87ab7af3fba73e
lc1006 <1176168734@qq.com>
2024-11-18 14:59:43 +0800

    feat:update upm

---
+++ Runtime/Public/TapTapSdk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using TapSDK.Core;
+using TapSDK.Core.Internal;
+using UnityEngine;
+
+namespace TapSDK.Login.Internal
+{
+    public static class TapTapSdk
+    {
+        public static string ClientId { get; private set; }
+
+        public static Region CurrentRegion { get; private set; }
+        
+        public static void SDKInitialize(string clientId, bool isCn) {
+            if (string.IsNullOrEmpty(clientId))
+                throw new ArgumentException("[TapSDK] clientId is null or empty!");
+            ClientId = clientId;
+            CurrentRegion = isCn ? (Region)new RegionCN() : new RegionIO();
+            TapLocalizeManager.SetCurrentRegion(isCn);
+        }
+    }
+}
